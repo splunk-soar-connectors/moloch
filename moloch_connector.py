1e82224a982b874eda76672855133229cd0b370d
farhanbhoraniya <farhan.bhoraniya@crestdatasys.com>
2018-02-16 12:18:30 +0530

    PAFM-2 : create-basic-structure-of-app-and-test-connectivity

---
+++ moloch_connector.py
@@ -0,0 +1,479 @@
+# --
+# File: moloch_connector.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2018
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber Corporation.
+#
+# --
+
+
+# Phantom App imports
+import phantom.app as phantom
+from phantom.base_connector import BaseConnector
+from phantom.action_result import ActionResult
+
+from moloch_consts import *
+import requests
+import json
+from requests.auth import HTTPDigestAuth
+from bs4 import BeautifulSoup
+
+
+class RetVal(tuple):
+    def __new__(cls, val1, val2):
+        return tuple.__new__(RetVal, (val1, val2))
+
+
+class MolochConnector(BaseConnector):
+
+    def __init__(self):
+
+        # Call the BaseConnectors init first
+        super(MolochConnector, self).__init__()
+
+        self._state = None
+        self._server_url = None
+        self._port = None
+        self._username = None
+        self._password = None
+        self._verify_server_cert = False
+
+    def initialize(self):
+        """ This is an optional function that can be implemented by the AppConnector derived class. Since the
+        configuration dictionary is already validated by the time this function is called, it's a good place to do any
+        extra initialization of any internal modules. This function MUST return a value of either phantom.APP_SUCCESS or
+        phantom.APP_ERROR. If this function returns phantom.APP_ERROR, then AppConnector::handle_action will not get
+        called.
+        """
+
+        self._state = self.load_state()
+
+        # get the asset config
+        config = self.get_config()
+
+        # Access values in asset config by the name
+        self._server_url = config[MOLOCH_CONFIG_SERVER_URL].strip('/')
+        self._port = int(config.get(MOLOCH_CONFIG_PORT, 8005))
+        self._username = config[MOLOCH_CONFIG_USERNAME]
+        self._password = config[MOLOCH_CONFIG_PASSWORD]
+        self._verify_server_cert = config.get(MOLOCH_VERIFY_SERVER_CERT, False)
+
+        return phantom.APP_SUCCESS
+
+    def _process_empty_reponse(self, response, action_result):
+        """ This function is used to process empty response.
+
+        :param response: response data
+        :param action_result: object of Action Result
+        :return: status phantom.APP_ERROR/phantom.APP_SUCCESS(along with appropriate message)
+        """
+
+        if response.status_code == 200:
+            return RetVal(phantom.APP_SUCCESS, {})
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, "Empty response and no information in the header"),
+                      None)
+
+    def _process_html_response(self, response, action_result):
+        """ This function is used to process html response.
+
+        :param response: response data
+        :param action_result: object of Action Result
+        :return: status phantom.APP_ERROR/phantom.APP_SUCCESS(along with appropriate message)
+        """
+
+        # An html response, treat it like an error
+        status_code = response.status_code
+
+        try:
+            soup = BeautifulSoup(response.text, "html.parser")
+            error_text = soup.text
+            split_lines = error_text.split('\n')
+            split_lines = [x.strip() for x in split_lines if x.strip()]
+            error_text = '\n'.join(split_lines)
+        except:
+            error_text = "Cannot parse error details"
+
+        message = "Status Code: {0}. Data from server:\n{1}\n".format(status_code, error_text)
+
+        message = message.replace('{', '{{').replace('}', '}}')
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _process_json_response(self, response, action_result):
+        """ This function is used to process json response.
+
+        :param response: response data
+        :param action_result: object of Action Result
+        :return: status phantom.APP_ERROR/phantom.APP_SUCCESS(along with appropriate message)
+        """
+
+        # Try a json parse
+        try:
+            resp_json = response.json()
+        except Exception as e:
+            return RetVal(action_result.set_status(phantom.APP_ERROR, "Unable to parse JSON response. Error: {0}".
+                                                   format(str(e))), None)
+
+        # Please specify the status codes here
+        if 200 <= response.status_code < 399:
+            return RetVal(phantom.APP_SUCCESS, resp_json)
+
+        # You should process the error returned in the json
+        message = "Error from server. Status Code: {0} Data from server: {1}".format(response.status_code,
+                                                                                     response.text.replace('{', '{{').
+                                                                                     replace('}', '}}'))
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _process_response(self, response, action_result):
+        """ This function is used to process html response.
+
+        :param response: response data
+        :param action_result: object of Action Result
+        :return: status phantom.APP_ERROR/phantom.APP_SUCCESS(along with appropriate message)
+        """
+
+        # store the r_text in debug data, it will get dumped in the logs if the action fails
+        if hasattr(action_result, 'add_debug_data'):
+            action_result.add_debug_data({'r_status_code': response.status_code})
+            action_result.add_debug_data({'r_text': response.text})
+            action_result.add_debug_data({'r_headers': response.headers})
+
+        # Process each 'Content-Type' of response separately
+
+        # Process a json response
+        if 'json' in response.headers.get('Content-Type', ''):
+            return self._process_json_response(response, action_result)
+
+        # Process an HTML resonse, Do this no matter what the API talks.
+        # There is a high chance of a PROXY in between phantom and the rest of
+        # world, in case of errors, PROXY's return HTML, this function parses
+        # the error and adds it to the action_result.
+        if 'html' in response.headers.get('Content-Type', ''):
+            return self._process_html_response(response, action_result)
+
+        # it's not content-type that is to be parsed, handle an empty response
+        if not response.text:
+            return self._process_empty_reponse(response, action_result)
+
+        # everything else is actually an error at this point
+        message = "Can't process response from server. Status Code: {0} Data from server: {1}".\
+            format(response.status_code, response.text.replace('{', '{{').replace('}', '}}'))
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _make_rest_call(self, endpoint, action_result, headers=None, params=None, data=None, method="get",
+                        timeout=None):
+        """ Function that makes the REST call to the device. It's a generic function that can be called from various
+            action handlers.
+
+        :param endpoint: REST endpoint that needs to appended to the service address
+        :param action_result: object of ActionResult class
+        :param headers: request headers
+        :param params: request parameters
+        :param data: request body
+        :param method: GET/POST/PUT/DELETE (Default will be GET)
+        :param timeout: Timeout for API call
+        :return: status phantom.APP_ERROR/phantom.APP_SUCCESS(along with appropriate message),
+        response obtained by making an API call
+        """
+
+        config = self.get_config()
+
+        resp_json = None
+
+        try:
+            request_func = getattr(requests, method)
+        except AttributeError:
+            return RetVal(action_result.set_status(phantom.APP_ERROR, "Invalid method: {0}".format(method)), resp_json)
+
+        # Create a URL to connect to
+        url = self._server_url + endpoint
+
+        try:
+            r = request_func(url, auth=HTTPDigestAuth(self._username, self._password), json=data, headers=headers,
+                             verify=config.get('verify_server_cert', False), timeout=timeout, params=params)
+        except Exception as e:
+            return RetVal(action_result.set_status(phantom.APP_ERROR, "Error Connecting to server. Details: {0}".
+                                                   format(str(e))), resp_json)
+
+        return self._process_response(r, action_result)
+
+    def _handle_test_connectivity(self, param):
+        """ This function is used to test the connectivity of an asset with given credentials.
+
+        :param param: (not used in this method)
+        :return: status success/failure
+        """
+
+        action_result = self.add_action_result(ActionResult(dict(param)))
+        self.save_progress(MOLOCH_TEST_CONNECTION)
+
+        params = {'length': 1}
+        endpoint = ':{port}{endpoint}'.format(port=self._port, endpoint=MOLOCH_TEST_CONNECTIVITY_ENDPOINT)
+
+        # make rest call
+        ret_val, response = self._make_rest_call(endpoint=endpoint, params=params, action_result=action_result,
+                                                 timeout=MOLOCH_TEST_CONNECTIVITY_TIMEOUT)
+
+        if phantom.is_fail(ret_val):
+            self.save_progress(MOLOCH_TEST_CONNECTIVITY_FAILED)
+            return action_result.get_status()
+
+        self.save_progress(MOLOCH_TEST_CONNECTIVITY_PASSED)
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def _handle_get_pcap(self, param):
+
+        # Implement the handler here
+        # use self.save_progress(...) to send progress messages back to the platform
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+
+        # Add an action result object to self (BaseConnector) to represent the action for this param
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        """
+        # Access action parameters passed in the 'param' dictionary
+
+        # Required values can be accessed directly
+        required_parameter = param['required_parameter']
+
+        # Optional values should use the .get() function
+        optional_parameter = param.get('optional_parameter', 'default_value')
+        """
+
+        """
+        # make rest call
+        ret_val, response = self._make_rest_call('/endpoint', action_result, params=None, headers=None)
+
+        if (phantom.is_fail(ret_val)):
+            # the call to the 3rd party device or service failed, action result should contain all the error details
+            # so just return from here
+            return action_result.get_status()
+
+        # Now post process the data,  uncomment code as you deem fit
+
+        # Add the response into the data section
+        # action_result.add_data(response)
+        """
+
+        action_result.add_data({})
+
+        # Add a dictionary that is made up of the most important values from data into the summary
+        summary = action_result.update_summary({})
+        summary['important_data'] = "value"
+
+        # Return success, no need to set the message, only the status
+        # BaseConnector will create a textual message based off of the summary dictionary
+        # return action_result.set_status(phantom.APP_SUCCESS)
+
+        # For now return Error with a message, in case of success we don't set the message, but use the summary
+        return action_result.set_status(phantom.APP_ERROR, "Action not yet implemented")
+
+    def _handle_list_fields(self, param):
+
+        # Implement the handler here
+        # use self.save_progress(...) to send progress messages back to the platform
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+
+        # Add an action result object to self (BaseConnector) to represent the action for this param
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        """
+        # Access action parameters passed in the 'param' dictionary
+
+        # Required values can be accessed directly
+        required_parameter = param['required_parameter']
+
+        # Optional values should use the .get() function
+        optional_parameter = param.get('optional_parameter', 'default_value')
+        """
+
+        """
+        # make rest call
+        ret_val, response = self._make_rest_call('/endpoint', action_result, params=None, headers=None)
+
+        if (phantom.is_fail(ret_val)):
+            # the call to the 3rd party device or service failed, action result should contain all the error details
+            # so just return from here
+            return action_result.get_status()
+
+        # Now post process the data,  uncomment code as you deem fit
+
+        # Add the response into the data section
+        # action_result.add_data(response)
+        """
+
+        action_result.add_data({})
+
+        # Add a dictionary that is made up of the most important values from data into the summary
+        summary = action_result.update_summary({})
+        summary['important_data'] = "value"
+
+        # Return success, no need to set the message, only the status
+        # BaseConnector will create a textual message based off of the summary dictionary
+        # return action_result.set_status(phantom.APP_SUCCESS)
+
+        # For now return Error with a message, in case of success we don't set the message, but use the summary
+        return action_result.set_status(phantom.APP_ERROR, "Action not yet implemented")
+
+    def _handle_list_files(self, param):
+
+        # Implement the handler here
+        # use self.save_progress(...) to send progress messages back to the platform
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+
+        # Add an action result object to self (BaseConnector) to represent the action for this param
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        """
+        # Access action parameters passed in the 'param' dictionary
+
+        # Required values can be accessed directly
+        required_parameter = param['required_parameter']
+
+        # Optional values should use the .get() function
+        optional_parameter = param.get('optional_parameter', 'default_value')
+        """
+
+        """
+        # make rest call
+        ret_val, response = self._make_rest_call('/endpoint', action_result, params=None, headers=None)
+
+        if (phantom.is_fail(ret_val)):
+            # the call to the 3rd party device or service failed, action result should contain all the error details
+            # so just return from here
+            return action_result.get_status()
+
+        # Now post process the data,  uncomment code as you deem fit
+
+        # Add the response into the data section
+        # action_result.add_data(response)
+        """
+
+        action_result.add_data({})
+
+        # Add a dictionary that is made up of the most important values from data into the summary
+        summary = action_result.update_summary({})
+        summary['important_data'] = "value"
+
+        # Return success, no need to set the message, only the status
+        # BaseConnector will create a textual message based off of the summary dictionary
+        # return action_result.set_status(phantom.APP_SUCCESS)
+
+        # For now return Error with a message, in case of success we don't set the message, but use the summary
+        return action_result.set_status(phantom.APP_ERROR, "Action not yet implemented")
+
+    def handle_action(self, param):
+        """ This function gets current action identifier and calls member function of its own to handle the action.
+
+        :param param: dictionary which contains information about the actions to be executed
+        :return: status success/failure
+        """
+
+        self.debug_print("action_id", self.get_action_identifier())
+
+        # Dictionary mapping each action with its corresponding actions
+        action_mapping = {
+            'test_connectivity': self._handle_test_connectivity,
+            'get_pcap': self._handle_get_pcap,
+            'list_files': self._handle_list_files,
+            'list_fields': self._handle_list_fields
+        }
+
+        action = self.get_action_identifier()
+        action_execution_status = phantom.APP_SUCCESS
+
+        if action in action_mapping.keys():
+            action_function = action_mapping[action]
+            action_execution_status = action_function(param)
+
+        return action_execution_status
+
+    def finalize(self):
+        """ This function gets called once all the param dictionary elements are looped over and no more handle_action
+        calls are left to be made. It gives the AppConnector a chance to loop through all the results that were
+        accumulated by multiple handle_action function calls and create any summary if required. Another usage is
+        cleanup, disconnect from remote devices etc.
+
+        :return: status (success/failure)
+        """
+
+        self.save_state(self._state)
+        return phantom.APP_SUCCESS
+
+
+if __name__ == '__main__':
+
+    import sys
+    import pudb
+    import argparse
+
+    pudb.set_trace()
+
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument('input_test_json', help='Input Test JSON file')
+    argparser.add_argument('-u', '--username', help='username', required=False)
+    argparser.add_argument('-p', '--password', help='password', required=False)
+
+    args = argparser.parse_args()
+    session_id = None
+
+    username = args.username
+    password = args.password
+
+    if username is not None and password is None:
+
+        # User specified a username but not a password, so ask
+        import getpass
+        password = getpass.getpass("Password: ")
+
+    if username and password:
+        try:
+            print ("Accessing the Login page")
+            r = requests.get("https://127.0.0.1/login", verify=False)
+            csrftoken = r.cookies['csrftoken']
+
+            data = dict()
+            data['username'] = username
+            data['password'] = password
+            data['csrfmiddlewaretoken'] = csrftoken
+
+            headers = dict()
+            headers['Cookie'] = 'csrftoken={}'.format(csrftoken)
+            headers['Referer'] = 'https://127.0.0.1/login'
+
+            print ("Logging into Platform to get the session id")
+            r2 = requests.post("https://127.0.0.1/login", verify=False, data=data, headers=headers)
+            session_id = r2.cookies['sessionid']
+        except Exception as e:
+            print ("Unable to get session id from the platfrom. Error: {}".format(str(e)))
+            exit(1)
+
+    if len(sys.argv) < 2:
+        print "No test json specified as input"
+        exit(0)
+
+    with open(sys.argv[1]) as f:
+        in_json = f.read()
+        in_json = json.loads(in_json)
+        print(json.dumps(in_json, indent=4))
+
+        connector = MolochConnector()
+        connector.print_progress_message = True
+
+        if session_id is not None:
+            in_json['user_session_token'] = session_id
+
+        ret_val = connector._handle_action(json.dumps(in_json), None)
+        print (json.dumps(json.loads(ret_val), indent=4))
+
+    exit(0)
---
+++ moloch_consts.py
@@ -0,0 +1,24 @@
+# --
+# File: moloch_consts.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2018
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber Corporation.
+#
+# --
+
+MOLOCH_CONFIG_SERVER_URL = 'server_url'
+MOLOCH_CONFIG_PORT = 'port'
+MOLOCH_CONFIG_USERNAME = 'username'
+MOLOCH_CONFIG_PASSWORD = 'password'
+MOLOCH_VERIFY_SERVER_CERT = 'verify_server_cert'
+MOLOCH_TEST_CONNECTION = 'Querying endpoint to verify the credentials provided'
+MOLOCH_TEST_CONNECTIVITY_FAILED = 'Test Connectivity Failed'
+MOLOCH_TEST_CONNECTIVITY_PASSED = 'Test Connectivity Passed'
+MOLOCH_TEST_CONNECTIVITY_ENDPOINT = '/sessions.json'
+MOLOCH_TEST_CONNECTIVITY_TIMEOUT = 30
