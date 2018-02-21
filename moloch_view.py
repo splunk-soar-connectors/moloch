0af3ef4e648dc0e7f79e428f7947b5a28ba63f71
farhanbhoraniya_crest <farhan.bhoraniya@crestdatasys.com>
2018-02-21 19:30:32 +0530

    PAFM-84 : get-pcap-action-with-incorrect

--- moloch_view.py
+++ moloch_view.py
@@ -30,7 +30,7 @@ def get_ctx_result(provides, result):
     data = result.get_data()
 
     if provides == 'get pcap':
-        param['start_time'] = '{}Z'.format(datetime.utcfromtimestamp(int(param['start_time'])))
+        param['start_time'] = '{}Z'.format(datetime.utcfromtimestamp(int(param['start_time'])).isoformat())
         param['end_time'] = '{}Z'.format(datetime.utcfromtimestamp(int(param['end_time'])).isoformat())
 
     ctx_result['param'] = param
